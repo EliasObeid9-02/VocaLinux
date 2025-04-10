5d786d3ba3ddb33516486abc33e5c0237807e0b4
Elias Obeid <elias.obeid9.02@gmail.com>
2025-04-10 11:02:36 +0300

    feat(interpreter): implemented errors and command parser

---
+++ src/interpreter/error.py
@@ -0,0 +1,26 @@
+class Error:
+    error_message: str | None
+    internal_error: str | None
+
+    def is_empty(self) -> bool:
+        return self.error_message is None and self.internal_error is None
+
+    def __init__(
+        self, error_message: str | None = None, internal_error: str | None = None
+    ):
+        self.error_message = error_message
+        self.internal_error = internal_error
+
+    def __str__(self) -> str:
+        return self.error_message or "Empty error"
+
+
+empty_err = Error()
+
+
+def generate_invalid_command_err(text: str) -> Error:
+    return Error(
+        error_message="Invalid command. Please provide a supported command.",
+        internal_error=f"The provided text (%s) is not in the list of supported commands"
+        % text,
+    )
---
+++ src/interpreter/parse_command.py
@@ -0,0 +1,20 @@
+from typing import cast
+
+from interpreter.error import Error, empty_err, generate_invalid_command_err
+
+commands = {
+    "change directory": "cd",
+    "copy": "cp",
+    "echo": "echo",
+    "list": "ls",
+    "make directory": "mkdir",
+    "move": "mv",
+    "remove": "rm",
+    "touch": "touch",
+}
+
+
+def parse_command(text: str) -> tuple[str, Error]:
+    if commands.get(text) is None:
+        return "", generate_invalid_command_err(text)
+    return cast(str, commands.get(text)), empty_err
