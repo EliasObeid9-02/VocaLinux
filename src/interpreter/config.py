7b44c5aa4019227f78c5fc8f32806ca409882e6f
Elias Obeid <elias.obeid9.02@gmail.com>
2025-08-17 22:50:03 +0300

    feat(interpreter): added interpreter recognized keywords

---
+++ src/interpreter/config.py
@@ -0,0 +1,88 @@
+"""
+This module contains the configuration for the interpreter.
+It includes the vocabulary and mappings used for translating spoken words into text.
+"""
+
+from typing import Dict, List
+
+PHONETIC_ALPHABET: Dict[str, str] = {
+    "adam": "a",
+    "boy": "b",
+    "charlie": "c",
+    "david": "d",
+    "edward": "e",
+    "frank": "f",
+    "george": "g",
+    "henry": "h",
+    "ida": "i",
+    "john": "j",
+    "king": "k",
+    "lincoln": "l",
+    "mary": "m",
+    "nora": "n",
+    "ocean": "o",
+    "paul": "p",
+    "queen": "q",
+    "robert": "r",
+    "sam": "s",
+    "tom": "t",
+    "union": "u",
+    "victor": "v",
+    "william": "w",
+    "x-ray": "x",
+    "young": "y",
+    "zebra": "z",
+}
+
+SPECIAL_CHARS: Dict[str, str] = {
+    "semicolon": ";",
+    "slash": "/",
+    "double quote": '"',
+    "single quote": "'",
+    "dash": "-",
+    "hyphen": "-",
+    "dot": ".",
+    "double dot": "..",
+    "space": " ",
+    "pipe": "|",
+    "greater than": ">",
+    "underscore": "_",
+}
+
+DIGIT_WORDS: Dict[str, str] = {
+    "zero": "0",
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
+}
+
+ACCEPTED_COMMANDS: List[str] = [
+    "echo",
+    "touch",
+    "ls",
+    "cd",
+    "mv",
+    "cp",
+    "rm",
+    "mkdir",
+    "rmdir",
+]
+
+VOCABULARY: List[str] = (
+    list(PHONETIC_ALPHABET.keys())
+    + list(SPECIAL_CHARS.keys())
+    + list(DIGIT_WORDS.keys())
+    + ACCEPTED_COMMANDS
+)
+
+MAPPINGS: Dict[str, str] = {
+    **PHONETIC_ALPHABET,
+    **SPECIAL_CHARS,
+    **DIGIT_WORDS,
+}
